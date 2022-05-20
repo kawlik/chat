15d2f31f0676f9dfa15eb7afb6fd02f6d5aa5fcb
kawlik <kpawlik1103@gmail.com>
2022-05-20 18:48:09 +0200

    Seting up PWA

--- src/main.tsx
+++ src/main.tsx
@@ -9,17 +9,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/main.scss';
 
 //  Routing
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 
 /*  Render App
 /*   *   *   *   *   *   *   *   *   *   */
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
 
         <App />
 
-    </HashRouter>
+    </BrowserRouter>
     </React.StrictMode>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
+
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 
 /*  Define Vite Config
@@ -8,6 +10,71 @@ export default defineConfig({
 
     //  used plugins
     plugins: [
-        react()
+        react(),
+        VitePWA({
+
+            //  PWA mode
+            registerType: 'autoUpdate',
+
+            //  workbox options
+            workbox: {
+                cleanupOutdatedCaches: false, 
+            },
+
+            //  included assets
+            includeAssets: [
+                'favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png',
+            ],
+
+            //  manifest
+            manifest: {
+
+                name: 'Chat!',
+                short_name: 'Chat!',
+
+                display: 'standalone',
+                theme_color: '#ffffff',
+                background_color: '#ffffff',
+                orientation: 'portrait',
+
+                description: 'Chat! A simple PWA app for training purpose!',
+
+                icons: [
+                    {
+                        src: 'android-chrome-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'android-chrome-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'android-chrome-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'any maskable',
+                    },
+                ],
+                // screenshots: [
+                //     {
+                //         src: 'screenshot-01.jpg',
+                //         sizes: '1080x2340',
+                //         type: 'image/jpg',
+                //     },
+                //     {
+                //         src: 'screenshot-02.jpg',
+                //         sizes: '1080x2340',
+                //         type: 'image/jpg',
+                //     },
+                //     {
+                //         src: 'screenshot-03.jpg',
+                //         sizes: '1080x2340',
+                //         type: 'image/jpg',
+                //     },
+                // ],
+            },
+        })
     ],
 });
